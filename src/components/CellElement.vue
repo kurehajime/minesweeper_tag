12f7fdcb5d94f9442ef6f05ee558f58d702a8ba8
kurehajime <xiidec@gmail.com>
2022-12-29 13:20:35 +0900

    属性はケバブケースだった。

--- src/components/CellElement.vue
+++ src/components/CellElement.vue
@@ -24,21 +24,21 @@ const mark = (): string => {
 <template>
     <g>
         <rect :x="props.x" :y="props.y" :width="cellSize" :height="cellSize" stroke='black' fill='whitesmoke' />
-        <text :x="props.x + cellSize / 2" :y="props.y + cellSize / 2" textAnchor="middle" dominantBaseline="central"
+        <text :x="props.x + cellSize / 2" :y="props.y + cellSize / 2" text-anchor="middle" dominant-baseline="central"
             stroke="black">{{ mark() }}</text>
         <g :display="props.cell.Open ? 'none' : 'block'">
             <rect :x="props.x" :y="props.y" :width="cellSize" :height="cellSize" stroke='black' fill='lightgray' />
             <line :x1="props.x + 0" :y1="props.y + 0" :x2="props.x + cellSize" :y2="props.y + 0"
-                :stroke="props.selected ? 'blue' : 'white'" strokeWidth="3" />
+                :stroke="props.selected ? 'blue' : 'white'" stroke-width="3" />
             <line :x1="props.x + cellSize - 3" :y1="props.y + 0" :x2="props.x + cellSize - 3" :y2="props.y + cellSize"
-                :stroke="props.selected ? 'blue' : 'gray'" strokeWidth=" 3" />
+                :stroke="props.selected ? 'blue' : 'gray'" stroke-width=" 3" />
             <line :x1="props.x + cellSize - 3" :y1="props.y + cellSize - 3" :x2="props.x + 0"
-                :y2="props.y + cellSize - 3" :stroke="props.selected ? 'blue' : 'gray'" strokeWidth="3" />
+                :y2="props.y + cellSize - 3" :stroke="props.selected ? 'blue' : 'gray'" stroke-width="3" />
             <line :x1="props.x + 0" :y1="props.y + cellSize" :x2="props.x + 0" :y2="props.y + 0"
-                :stroke="props.selected ? 'blue' : 'white'" strokeWidth="3" />
+                :stroke="props.selected ? 'blue' : 'white'" stroke-width="3" />
         </g>
         <text :display="props.cell.Flag ? 'block' : 'none'" :x="props.x + cellSize / 2" :y="props.y + cellSize / 2"
-            textAnchor="middle" dominantBaseline="central" stroke="black">🚩</text>
+            text-anchor="middle" dominant-baseline="central" stroke="black">🚩</text>
     </g>
 </template>
 <style scoped>
